17ef0f7848b22ccaaab2bd009a6dae67b00158da
Dani <danihazler@gmail.com>
2018-10-18 15:13:40 +0100

    Add product to the cart, part 1

--- main.js
+++ main.js
@@ -9,16 +9,57 @@ const productList = [
   {name:"Energy Drink", price: 4, image:"energy-drink.svg", qty: 25}
 ];
 const products = document.getElementById('products');
+const cartDOM = document.querySelector('.cart');
+
+let cart = [];
 
 
 /* ***** PRODUCT DIV ******
 **************************** */
 productList.map(product => {
-  let output = `<div class="product">
-  <img class="product__image" src="img/products/${product.image}" alt="${product.name}">
-  <h2 class="product__name">${product.name}</h2>
-  <h3 class="product__price">${product.price}</h3>
-  <button class="btn btn--primary" data-action="ADD_TO_CART">Add To Cart</button>
-  </div>`
+  let output = `
+    <div class="product">
+      <img class="product__image" src="img/products/${product.image}" alt="${product.name}">
+      <h2 class="product__name">${product.name}</h2>
+      <h3 class="product__price">${product.price}</h3>
+      <button class="btn btn--primary" data-action="ADD_TO_CART">Add To Cart</button>
+    </div>`
   products.innerHTML += output;
+});
+
+/* ***** ADD TO CART BUTTON ******
+**************************** */
+
+/* this variable needs to be created after the creation
+of the HTML elements, that's why it's not on the top */
+const addToCartButtonsDOM = document.querySelectorAll("[data-action='ADD_TO_CART']");
+
+addToCartButtonsDOM.forEach(addToCartButtonsDOM => {
+  addToCartButtonsDOM.addEventListener("click", () => {
+    const productDOM = addToCartButtonsDOM.parentNode;
+
+    const product = {
+      image: productDOM.querySelector(".product__image").getAttribute("src"),
+      name: productDOM.querySelector(".product__name").innerText,
+      price: productDOM.querySelector(".product__price").innerText,
+      quantity: 1
+    };
+    const addedToCart = (cart.filter(cartItem => (cartItem.name === product.name)).length > 0);
+
+    if (!addedToCart) {
+      cartDOM.insertAdjacentHTML('beforeend', `
+        <div class="cart__item">
+          <img class="cart__item__image" src="${product.image}" alt="${product.name}">
+          <h3 class="cart__item__name">${product.name}</h3>
+          <h3 class="cart__item__price">${product.price}</h3>
+          <button class="btn btn--primary btn--small" data-action="DECREASE_ITEM">&minus;</button>
+          <h3 class="cart__item__quantity">${product.quantity}</h3>
+          <button class="btn btn--primary btn--small" data-action="INCREASE_ITEM">&plus;</button>
+          <button class="btn btn--danger btn--small" data-action="REMOVE_ITEM">&minus;</button>
+        </div>`
+      );
+      cart.push(product);
+      addToCartButtonsDOM.innerText = 'In Cart';
+    }
+  });
 });
