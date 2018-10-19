97630742ae67148070b943f6f6faa75311916e14
Dani <danihazler@gmail.com>
2018-10-19 10:32:48 +0100

    Add a trailing zero to the displayed price

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const productList = [
   {name:"Cocktail", price: 8.50, image:"cocktail.svg", qty: 25},
   {name:"Cola", price: 0.90, image:"cola.svg", qty: 10},
   {name:"Dessert Wine", price: 4.25, image:"dessert-wine.svg", qty: 15},
-  {name:"Energy Drink", price: 4, image:"energy-drink.svg", qty: 25}
+  {name:"Energy Drink", price: 4.00, image:"energy-drink.svg", qty: 25}
 ];
 const products = document.getElementById('products');
 const cartDOM = document.querySelector('.cart');
@@ -20,7 +20,7 @@ productList.map(product => {
     <div class="product">
       <img class="product__image" src="img/products/${product.image}" alt="${product.name}">
       <h2 class="product__name">${product.name}</h2>
-      <h3 class="product__price">${product.price}</h3>
+      <h3 class="product__price">${product.price.toFixed(2)}</h3>
       <button class="btn btn--primary" data-action="ADD_TO_CART">Add To Cart</button>
     </div>`
   products.innerHTML += output;
